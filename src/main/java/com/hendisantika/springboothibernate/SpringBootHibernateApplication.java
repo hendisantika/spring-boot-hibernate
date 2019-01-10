1e3726e110b587186ad3a61762d57a3ccba62678
Hendi Santika <hendisantika@gmail.com>
2019-01-11 05:44:23 +0700

    #9 - Enable JPA Auditing in Main App

--- src/main/java/com/hendisantika/springboothibernate/SpringBootHibernateApplication.java
+++ src/main/java/com/hendisantika/springboothibernate/SpringBootHibernateApplication.java
@@ -2,8 +2,13 @@ package com.hendisantika.springboothibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 public class SpringBootHibernateApplication {
 
     public static void main(String[] args) {
