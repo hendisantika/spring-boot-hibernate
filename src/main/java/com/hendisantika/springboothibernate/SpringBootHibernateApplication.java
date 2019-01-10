89cb0a022ef0679e3a8b1cbeb1a19b60413c2a87
Hendi Santika <hendisantika@gmail.com>
2019-01-11 05:37:39 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboothibernate/SpringBootHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springboothibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateApplication.class, args);
+    }
+
+}
+
